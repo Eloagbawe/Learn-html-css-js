ce8993573147bf6b590fbc53d06cf484dd2c1b3c
Eloagbawe <eloagbawe@gmail.com>
2020-05-10 23:56:25 +0100

    program structure

---
+++ program-structure.js
@@ -0,0 +1,25 @@
+let result = 1;
+let counter = 0;
+
+//while loop
+while (counter < 10 ){
+    result = result * 2;
+    counter = counter + 1;
+}
+console.log(result);
+
+//do while loop
+let res = 1;
+let count = 0;
+do{
+    res = res * 2;
+    count = count + 1;
+} while (count < 10);
+console.log(res);
+
+//for loop
+let answer = 1;
+for (let i = 0; i < 10; i++){
+    answer = answer * 2; 
+}
+console.log(answer);
