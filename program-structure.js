28d34aefb0e29113e535aff7b5ae4cf7d7df4f42
Eloagbawe <eloagbawe@gmail.com>
2020-05-11 11:46:43 +0100

    program structure

--- program-structure.js
+++ program-structure.js
@@ -23,3 +23,44 @@ for (let i = 0; i < 10; i++){
     answer = answer * 2; 
 }
 console.log(answer);
+
+var x = "";
+var y = 0;
+while (y < 7){
+    x += "#";
+    y = y + 1;
+    console.log(x);
+}
+
+var a = "";
+for (let b = 1; b <= 100; b++){
+    if ((b % 3 === 0) && (b % 5 === 0)){
+        a +="FizzBuzz \n";
+    }
+    else if ((b % 5)=== 0){
+        a +="Buzz \n";
+    }
+    else if ((b % 3) === 0){
+        a +="Fizz \n";
+    }
+    else{
+        a += b + "\n";
+    }
+}
+console.log(a);
+
+var c = " ";
+
+
+for (var e = 0; e < 8; e++){
+    for( var f = 0; f < 8; f++){
+       if (((e + f) % 2) !== 0){
+            c += "#";
+     }
+       else{
+        c += " ";
+    }   
+ }   
+    c += "\n";
+}
+console.log(c);
